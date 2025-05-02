f34a8aad3c8c995deda924191dbc0fb193c6ad84
Build-1-0-0 <africancontent807@gmail.com>
2025-05-02 10:08:38 +0300

    Create Not found.tsx

---
+++ frontend/src/components/errors/Not found.tsx
@@ -0,0 +1,9 @@
+// src/components/errors/NotFound.tsx
+export const NotFound = () => (
+  <div className="error-boundary">
+    <h1 className="error-boundary__title">404 - Page Not Found</h1>
+    <p className="error-boundary__message">
+      The page you're looking for doesn't exist.
+    </p>
+  </div>
+);
