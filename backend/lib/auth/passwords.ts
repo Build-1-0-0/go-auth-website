3ebf0fa8f63f91dd90d148ec267283f5603c607a
Build-1-0-0 <africancontent807@gmail.com>
2025-05-03 19:32:48 +0300

    Update passwords.ts

--- backend/lib/auth/passwords.ts
+++ backend/lib/auth/passwords.ts
@@ -1,9 +1,23 @@
-import * as argon2 from 'argon2';
+import { argon2id } from '@noble/hashes/argon2';
+import type { Env } from '@src/types/env';
 
-export const hashPassword = async (password: string): Promise<string> => {
-  return await argon2.hash(password);
+export const hashPassword = async (password: string, env: Env): Promise<string> => {
+  const secret = new TextEncoder().encode(env.SESSION_SECRET);
+  return argon2id(password, secret, {
+    t: 2, // Iterations
+    m: 19456, // Memory in KB
+    p: 1, // Parallelism
+    dkLen: 32 // Key length
+  }).toString('hex');
 };
 
-export const verifyPassword = async (hash: string, password: string): Promise<boolean> => {
-  return await argon2.verify(hash, password);
+export const verifyPassword = async (hash: string, password: string, env: Env): Promise<boolean> => {
+  const secret = new TextEncoder().encode(env.SESSION_SECRET);
+  const computedHash = argon2id(password, secret, {
+    t: 2,
+    m: 19456,
+    p: 1,
+    dkLen: 32
+  }).toString('hex');
+  return hash === computedHash;
 };
