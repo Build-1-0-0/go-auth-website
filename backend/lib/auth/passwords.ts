dbad49ce62353765dc09f7e70b49c49109a80645
Build-1-0-0 <africancontent807@gmail.com>
2025-05-03 16:24:19 +0300

    Update passwords.ts

--- backend/lib/auth/passwords.ts
+++ backend/lib/auth/passwords.ts
@@ -1,12 +1,9 @@
 import * as argon2 from 'argon2';
 
 export const hashPassword = async (password: string): Promise<string> => {
-  return await argon2.hash(password, { type: argon2.argon2id });
+  return await argon2.hash(password);
 };
 
-export const verifyPassword = async (
-  hash: string,
-  password: string
-): Promise<boolean> => {
+export const verifyPassword = async (hash: string, password: string): Promise<boolean> => {
   return await argon2.verify(hash, password);
 };
