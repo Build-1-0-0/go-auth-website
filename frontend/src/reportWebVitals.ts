9af10e26faee36c44c9263715bd18824ab44f1a9
Build-1-0-0 <africancontent807@gmail.com>
2025-05-03 23:56:17 +0300

    Update reportWebVitals.ts

--- frontend/src/reportWebVitals.ts
+++ frontend/src/reportWebVitals.ts
@@ -1,13 +1,13 @@
 import { ReportHandler } from 'web-vitals';
-import { getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
+import { onCLS, onFID, onFCP, onLCP, onTTFB } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    getCLS(onPerfEntry);
-    getFID(onPerfEntry);
-    getFCP(onPerfEntry);
-    getLCP(onPerfEntry);
-    getTTFB(onPerfEntry);
+    onCLS(onPerfEntry);
+    onFID(onPerfEntry);
+    onFCP(onPerfEntry);
+    onLCP(onPerfEntry);
+    onTTFB(onPerfEntry);
   }
 };
 
