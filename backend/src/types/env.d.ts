122a62e52c36364009387d35557c700fe7d61757
Build-1-0-0 <africancontent807@gmail.com>
2025-05-03 16:49:01 +0300

    Update env.d.ts

--- backend/src/types/env.d.ts
+++ backend/src/types/env.d.ts
@@ -1,8 +1,7 @@
 /// <reference types="@cloudflare/workers-types" />
-import type { BlankInput, Env as HonoEnv } from 'hono/types';
 
 declare global {
-  interface Env extends HonoEnv {
+  interface Env {
     Bindings: {
       DB: D1Database;
       SESSION_SECRET: string;
