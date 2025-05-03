bb7c9751a01f98eb61570d2056727fc1fd139942
Build-1-0-0 <africancontent807@gmail.com>
2025-05-03 18:08:20 +0300

    Update sessions.ts

--- backend/lib/auth/sessions.ts
+++ backend/lib/auth/sessions.ts
@@ -1,12 +1,11 @@
 import type { Env } from '@src/types/env';
 import { nanoid } from 'nanoid';
 
-export const createSession = async (db: D1Database, userId: string): Promise<string> => {
+export const createSession = async (env: Env, userId: string): Promise<string> => {
   const sessionId = nanoid();
   const expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 24 * 7); // 1 week
 
-  await db
-    .prepare('INSERT INTO sessions (id, user_id, expires_at) VALUES (?, ?, ?)')
+  await env.DB.prepare('INSERT INTO sessions (id, user_id, expires_at) VALUES (?, ?, ?)')
     .bind(sessionId, userId, expiresAt.toISOString())
     .run();
 
