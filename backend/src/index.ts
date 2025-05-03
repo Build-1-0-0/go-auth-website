a8b627b41a7f76d32aafd377ea342fda9350cc35
Build-1-0-0 <africancontent807@gmail.com>
2025-05-04 00:21:26 +0300

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,20 @@
+// backend/index.ts
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 import auth from './routes/auth';
 import protectedRoutes from './routes/protected';
 import type { Env } from './types/env';
 
 const app = new Hono<{ Bindings: Env }>();
+
+app.use(
+  '*',
+  cors({
+    origin: ['https://go-auth-website.pages.dev', 'http://localhost:5173'],
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 
 app.get('/', (c) => c.text('API Running'));
 app.route('/auth', auth);
