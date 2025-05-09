0feffb1835ecb3133dcd1f90317980db111646b0
Build-1-0-0 <africancontent807@gmail.com>
2025-05-10 00:43:02 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 // tailwind.config.js
 module.exports = {
-  content: ['./frontend/src/**/*.{js,ts,jsx,tsx,html}'],
+  content: [
+    './index.html',
+    './frontend/src/**/*.{js,ts,jsx,tsx}',
+    // Add any other file paths that contain Tailwind classes
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
