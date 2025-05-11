a9e9c35b93f14776b10e7942cceafe3bd972a1ae
Build-1-0-0 <africancontent807@gmail.com>
2025-05-11 14:22:09 +0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,1 @@
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
-import postcssNesting from 'postcss-nesting'
 
-export default {
-  plugins: [postcssNesting, tailwindcss, autoprefixer],
-}
