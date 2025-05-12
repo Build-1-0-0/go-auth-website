30cabd909f632aaaac91f739a8ea9e4e9ee24001
Build-1-0-0 <africancontent807@gmail.com>
2025-05-12 20:48:19 +0300

    Update Layout.tsx

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -9,10 +9,10 @@ export default function Layout() {
   console.log('Current path:', location.pathname);
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-background text-foreground fade-in">
       <NotificationBanner />
       <Header />
-      <main className="flex-grow container mx-auto px-4 py-8">
+      <main className="flex-grow container mx-auto px-4 py-6">
         <Outlet />
       </main>
       <Footer />
