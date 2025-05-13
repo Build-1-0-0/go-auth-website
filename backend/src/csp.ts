9fac8995440213efc3a67533e01e673105ff6a6c
Build-1-0-0 <africancontent807@gmail.com>
2025-05-13 16:16:47 +0300

    Update csp.ts

--- backend/src/csp.ts
+++ backend/src/csp.ts
@@ -8,7 +8,7 @@ export default {
       'Content-Security-Policy',
       `default-src 'self'; script-src 'self' 'nonce-${nonce}' https://static.cloudflareinsights.com`
     );
-    const body = (await response.text()).replace('abcd1234-efgh-5678-ijkl-9012mnop3456', nonce);
+    const body = (await response.text()).replace("__CSP_NONCE__", nonce);
     return new Response(body, {
       headers: newHeaders,
       status: response.status,
