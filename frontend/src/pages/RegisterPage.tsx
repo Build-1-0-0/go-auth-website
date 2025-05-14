52ab7232dc06e6f7d5b65fd68bd9b2e775531cfe
Build-1-0-0 <africancontent807@gmail.com>
2025-05-14 15:01:34 +0300

    Update RegisterPage.tsx

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,8 +1,17 @@
-// frontend/src/pages/RegisterPage.tsx
+import { Helmet } from 'react-helmet-async';
 import AuthForm from '@/components/auth/AuthForm';
+import Layout from '@/components/layout/Layout';
 
 const RegisterPage: React.FC = () => {
-  return <AuthForm isLogin={false} />;
+  return (
+    <Layout>
+      <Helmet>
+        <title>Register - MyApp</title>
+        <meta name="description" content="Create a new account to access MyApp." />
+      </Helmet>
+      <AuthForm isLogin={false} />
+    </Layout>
+  );
 };
 
 export default RegisterPage;
