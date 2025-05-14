16769656e3eb45441c4f71c14f14d06d09746194
Build-1-0-0 <africancontent807@gmail.com>
2025-05-14 15:01:56 +0300

    Update LoginPage.tsx

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,8 +1,17 @@
-// frontend/src/pages/LoginPage.tsx
+import { Helmet } from 'react-helmet-async';
 import AuthForm from '@/components/auth/AuthForm';
+import Layout from '@/components/layout/Layout';
 
 const LoginPage: React.FC = () => {
-  return <AuthForm isLogin={true} />;
+  return (
+    <Layout>
+      <Helmet>
+        <title>Login - MyApp</title>
+        <meta name="description" content="Log in to your MyApp account." />
+      </Helmet>
+      <AuthForm isLogin={true} />
+    </Layout>
+  );
 };
 
 export default LoginPage;
